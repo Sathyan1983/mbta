7bbaa25b48d8d75314142a44cda8e9d2ec8d00c4
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-04-19 14:24:12 -0400

    Added method to Route model to indicate which name to display.

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,8 @@
 class Route < ActiveRecord::Base
   validates :agency_id, presence: true
   validates :obid, presence: true
+
+  def name
+    long_name.blank? ? "Route #{ self.short_name }" : long_name
+  end
 end
