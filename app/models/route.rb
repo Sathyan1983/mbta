d928f35418a3970b04f8dec6fc400f583d5ab60d
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-04-19 14:39:30 -0400

    changed 'name' method in route model to display name to be more expressive

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,7 +2,7 @@ class Route < ActiveRecord::Base
   validates :agency_id, presence: true
   validates :obid, presence: true
 
-  def name
+  def display_name
     long_name.blank? ? "Route #{ self.short_name }" : long_name
   end
 end
