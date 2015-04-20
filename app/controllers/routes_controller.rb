4c64dbe5a9e4fda63df6e2beb76b8efffe704010
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-04-20 11:41:04 -0400

    wrote method on the route show action to build a hash with the right latitudes and longitudes.

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -6,7 +6,10 @@ class RoutesController < ApplicationController
   def show
     route = Route.find(params[:id])
     @route = Route::GetBusPositions.call(route)
-    #@hash = Gmaps4rails.build_markers(@route.markers) do |bus, marker|
+    @hash = Gmaps4rails.build_markers(@route.buses) do |bus, marker|
+      marker.lat bus['lat']
+      marker.lng bus['lon']
+    end
     binding.pry
   end
 end
