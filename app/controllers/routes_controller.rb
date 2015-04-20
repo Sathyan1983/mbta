fb43f4f357ff5826f1242140aaf78649d9722b9c
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-04-20 13:22:02 -0400

    renamed the service object for getting the bus positions to 'add_bus_positions'.  Made appropriate changes to file name and controller.

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -5,7 +5,7 @@ class RoutesController < ApplicationController
 
   def show
     route = Route.find(params[:id])
-    @route = Route::GetBusPositions.call(route)
+    @route = Route::AddBusPositions.call(route)
     @hash = Gmaps4rails.build_markers(@route.buses) do |bus, marker|
       marker.lat bus['lat']
       marker.lng bus['lon']
